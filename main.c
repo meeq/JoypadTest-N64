b945c84a7e445e3c752cef0c6af7349c955db70b
Christopher Bonhage <me@christopherbonhage.com>
2022-03-04 12:24:46 -0500

    Reformat main.c

--- main.c
+++ main.c
@@ -9,29 +9,29 @@
 
 #include "joypad.h"
 
-const char * format_joypad_style(joypad_style_t style)
+const char *format_joypad_style(joypad_style_t style)
 {
-    switch ( style )
+    switch (style)
     {
-        case JOYPAD_STYLE_NONE:
-            return "None";
-        case JOYPAD_STYLE_N64:
-            return "N64";
-        case JOYPAD_STYLE_GCN:
-            return "GCN";
-        default:
-            return "Unknown";
+    case JOYPAD_STYLE_NONE:
+        return "None";
+    case JOYPAD_STYLE_N64:
+        return "N64";
+    case JOYPAD_STYLE_GCN:
+        return "GCN";
+    default:
+        return "Unknown";
     }
 }
 
-const char * format_joypad_rumble(bool supported, bool enabled)
+const char *format_joypad_rumble(bool supported, bool enabled)
 {
     if (!supported) return "Unavailable";
-    if (enabled) return "Rumbling";
+    if (enabled) return "Active";
     return "Idle";
 }
 
-void print_joypad_inputs( joypad_inputs_t inputs )
+void print_joypad_inputs(joypad_inputs_t inputs)
 {
     printf("Stick: %d,%d C-Stick: %d,%d L-Trig:%d R-Trig:%d\n", inputs.stick_x, inputs.stick_y, inputs.cstick_x, inputs.cstick_y, inputs.analog_l, inputs.analog_r);
     printf("A:%d B:%d X:%d Y:%d L:%d R:%d Z:%d Start:%d\n", inputs.a, inputs.b, inputs.x, inputs.y, inputs.l, inputs.r, inputs.z, inputs.start);
@@ -58,40 +58,40 @@ int main(void)
     joypad_inputs_t inputs;
 
     console_init();
-    console_set_render_mode( RENDER_MANUAL );
+    console_set_render_mode(RENDER_MANUAL);
 
     while (1)
     {
         console_clear();
 
-        printf( "N64/GCN Joypad Subsystem Test\n\n" );
+        printf("N64/GCN Joypad Subsystem Test\n\n");
 
         joypad_poll();
 
-        for ( joypad_port_t port = JOYPAD_PORT_1; port < JOYPAD_PORT_COUNT; ++port )
+        for (joypad_port_t port = JOYPAD_PORT_1; port < JOYPAD_PORT_COUNT; ++port)
         {
-            style = joypad_style( port );
-            rumble_supported = joypad_is_rumble_supported( port );
-            rumble_state = joypad_get_rumble_state( port );
-            inputs = joypad_inputs( port );
+            style = joypad_style(port);
+            rumble_supported = joypad_is_rumble_supported(port);
+            rumble_state = joypad_get_rumble_state(port);
+            inputs = joypad_inputs(port);
 
             if (rumble_supported)
             {
                 if (inputs.a && !rumble_state)
                 {
-                    joypad_set_rumble_state( port, true );
+                    joypad_set_rumble_state(port, true);
                 }
                 else if (!inputs.a && rumble_state)
                 {
-                    joypad_set_rumble_state( port, false );
+                    joypad_set_rumble_state(port, false);
                 }
             }
 
             printf("Port %d ", port + 1);
             printf("Style: %s ", format_joypad_style(style));
             printf("Rumble: %s\n", format_joypad_rumble(rumble_supported, rumble_state));
-            print_joypad_inputs( inputs );
-            printf( "\n" );
+            print_joypad_inputs(inputs);
+            printf("\n");
         }
 
         console_render();
