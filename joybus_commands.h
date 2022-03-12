9e2b16cbec485ac1c43b6a3ca7e073aca247614c
Christopher Bonhage <me@christopherbonhage.com>
2022-03-11 21:29:10 -0500

    Refactor joybus command structs to expose a union of send/recv bytes

--- joybus_commands.h
+++ joybus_commands.h
@@ -43,11 +43,25 @@ typedef struct __attribute__((packed)) joybus_cmd_n64_accessory_read_port_s
     uint8_t send_len;
     uint8_t recv_len;
     /* send data */
-    uint8_t command;
-    uint16_t addr_crc;
+    union
+    {
+        uint8_t send_bytes[0x03];
+        struct __attribute__((__packed__))
+        {
+            uint8_t command;
+            uint16_t addr_crc;
+        };
+    };
     /* recv_data */
-    uint8_t data[JOYBUS_N64_ACCESSORY_DATA_SIZE];
-    uint8_t data_crc;
+    union
+    {
+        uint8_t recv_bytes[0x21];
+        struct __attribute__((__packed__))
+        {
+            uint8_t data[JOYBUS_N64_ACCESSORY_DATA_SIZE];
+            uint8_t data_crc;
+        };
+    };
 } joybus_cmd_n64_accessory_read_port_t;
 
 typedef struct __attribute__((packed)) joybus_cmd_n64_accessory_write_port_s
@@ -56,11 +70,22 @@ typedef struct __attribute__((packed)) joybus_cmd_n64_accessory_write_port_s
     uint8_t send_len;
     uint8_t recv_len;
     /* send data */
-    uint8_t command;
-    uint16_t addr_crc;
-    uint8_t data[JOYBUS_N64_ACCESSORY_DATA_SIZE];
+    union
+    {
+        uint8_t send_bytes[0x23];
+        struct __attribute__((__packed__))
+        {
+            uint8_t command;
+            uint16_t addr_crc;
+            uint8_t data[JOYBUS_N64_ACCESSORY_DATA_SIZE];
+        };
+    };
     /* recv_data */
-    uint8_t data_crc;
+    union
+    {
+        uint8_t recv_bytes[0x01];
+        uint8_t data_crc;
+    };
 } joybus_cmd_n64_accessory_write_port_t;
 
 typedef struct __attribute__((packed)) joybus_cmd_identify_port_s
@@ -69,10 +94,21 @@ typedef struct __attribute__((packed)) joybus_cmd_identify_port_s
     uint8_t send_len;
     uint8_t recv_len;
     /* send data */
-    uint8_t command;
+    union
+    {
+        uint8_t send_bytes[0x01];
+        uint8_t command;
+    };
     /* recv data */
-    joybus_identifier_t identifier;
-    joybus_identify_status_t status;
+    union
+    {
+        uint8_t recv_bytes[0x03];
+        struct __attribute__((__packed__))
+        {
+            joybus_identifier_t identifier;
+            joybus_identify_status_t status;
+        };
+    };
 } joybus_cmd_identify_port_t;
 
 typedef struct __attribute__((packed)) joybus_cmd_n64_controller_read_port_s
@@ -81,26 +117,37 @@ typedef struct __attribute__((packed)) joybus_cmd_n64_controller_read_port_s
     uint8_t send_len;
     uint8_t recv_len;
     /* send data */
-    uint8_t command;
+    union
+    {
+        uint8_t send_bytes[0x01];
+        uint8_t command;
+    };
     /* recv data */
-    unsigned a : 1;
-    unsigned b : 1;
-    unsigned z : 1;
-    unsigned start : 1;
-    unsigned d_up : 1;
-    unsigned d_down : 1;
-    unsigned d_left : 1;
-    unsigned d_right : 1;
-    unsigned reset : 1;
-    unsigned : 1;
-    unsigned l : 1;
-    unsigned r : 1;
-    unsigned c_up : 1;
-    unsigned c_down : 1;
-    unsigned c_left : 1;
-    unsigned c_right : 1;
-    signed stick_x : 8;
-    signed stick_y : 8;
+    union
+    {
+        uint8_t recv_bytes[0x04];
+        struct __attribute__((__packed__))
+        {
+            unsigned a : 1;
+            unsigned b : 1;
+            unsigned z : 1;
+            unsigned start : 1;
+            unsigned d_up : 1;
+            unsigned d_down : 1;
+            unsigned d_left : 1;
+            unsigned d_right : 1;
+            unsigned reset : 1;
+            unsigned : 1;
+            unsigned l : 1;
+            unsigned r : 1;
+            unsigned c_up : 1;
+            unsigned c_down : 1;
+            unsigned c_left : 1;
+            unsigned c_right : 1;
+            signed stick_x : 8;
+            signed stick_y : 8;
+        };
+    };
 } joybus_cmd_n64_controller_read_port_t;
 
 typedef struct __attribute__((packed)) joybus_cmd_gcn_controller_origin_port_s
@@ -109,31 +156,42 @@ typedef struct __attribute__((packed)) joybus_cmd_gcn_controller_origin_port_s
     uint8_t send_len;
     uint8_t recv_len;
     /* send data */
-    uint8_t command;
+     union
+    {
+        uint8_t send_bytes[0x01];
+        uint8_t command;
+    };
     /* recv data */
-    unsigned err : 2;
-    unsigned origin_unchecked : 1;
-    unsigned start : 1;
-    unsigned y : 1;
-    unsigned x : 1;
-    unsigned b : 1;
-    unsigned a : 1;
-    unsigned : 1;
-    unsigned l : 1;
-    unsigned r : 1;
-    unsigned z : 1;
-    unsigned d_up : 1;
-    unsigned d_down : 1;
-    unsigned d_right : 1;
-    unsigned d_left : 1;
-    unsigned stick_x : 8;
-    unsigned stick_y : 8;
-    unsigned cstick_x : 8;
-    unsigned cstick_y : 8;
-    unsigned analog_l : 8;
-    unsigned analog_r : 8;
-    unsigned deadzone_0 : 8;
-    unsigned deadzone_1 : 8;
+    union
+    {
+        uint8_t recv_bytes[0x0A];
+        struct __attribute__((__packed__))
+        {
+            unsigned err : 2;
+            unsigned origin_unchecked : 1;
+            unsigned start : 1;
+            unsigned y : 1;
+            unsigned x : 1;
+            unsigned b : 1;
+            unsigned a : 1;
+            unsigned : 1;
+            unsigned l : 1;
+            unsigned r : 1;
+            unsigned z : 1;
+            unsigned d_up : 1;
+            unsigned d_down : 1;
+            unsigned d_right : 1;
+            unsigned d_left : 1;
+            unsigned stick_x : 8;
+            unsigned stick_y : 8;
+            unsigned cstick_x : 8;
+            unsigned cstick_y : 8;
+            unsigned analog_l : 8;
+            unsigned analog_r : 8;
+            unsigned deadzone_0 : 8;
+            unsigned deadzone_1 : 8;
+        };
+    };
 } joybus_cmd_gcn_controller_origin_port_t;
 
 typedef struct __attribute__((packed)) joybus_cmd_gcn_controller_read_port_s
@@ -142,31 +200,45 @@ typedef struct __attribute__((packed)) joybus_cmd_gcn_controller_read_port_s
     uint8_t send_len;
     uint8_t recv_len;
     /* send data */
-    uint8_t command;
-    uint8_t mode;
-    uint8_t rumble;
+    union
+    {
+        uint8_t send_bytes[0x03];
+        struct __attribute__((__packed__))
+        {
+            uint8_t command;
+            uint8_t mode;
+            uint8_t rumble;
+        };
+    };
     /* recv data */
-    unsigned err : 2;
-    unsigned origin_unchecked : 1;
-    unsigned start : 1;
-    unsigned y : 1;
-    unsigned x : 1;
-    unsigned b : 1;
-    unsigned a : 1;
-    unsigned : 1;
-    unsigned l : 1;
-    unsigned r : 1;
-    unsigned z : 1;
-    unsigned d_up : 1;
-    unsigned d_down : 1;
-    unsigned d_right : 1;
-    unsigned d_left : 1;
-    unsigned stick_x : 8;
-    unsigned stick_y : 8;
-    unsigned cstick_x : 8;
-    unsigned cstick_y : 8;
-    unsigned analog_l : 8;
-    unsigned analog_r : 8;
+    union
+    {
+        uint8_t recv_bytes[0x08];
+        struct __attribute__((__packed__))
+        {
+            unsigned err : 2;
+            unsigned origin_unchecked : 1;
+            unsigned start : 1;
+            unsigned y : 1;
+            unsigned x : 1;
+            unsigned b : 1;
+            unsigned a : 1;
+            unsigned : 1;
+            unsigned l : 1;
+            unsigned r : 1;
+            unsigned z : 1;
+            unsigned d_up : 1;
+            unsigned d_down : 1;
+            unsigned d_right : 1;
+            unsigned d_left : 1;
+            unsigned stick_x : 8;
+            unsigned stick_y : 8;
+            unsigned cstick_x : 8;
+            unsigned cstick_y : 8;
+            unsigned analog_l : 8;
+            unsigned analog_r : 8;
+        };
+    };
 } joybus_cmd_gcn_controller_read_port_t; 
 
 #ifdef __cplusplus
